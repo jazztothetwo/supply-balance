66fc40a08a6dee2f3a7ed0b5b3329a1c17fccde6
jazztothetwo <jarrodscherer@hotmail.com>
2026-09-01 00:14:58 +1000

    Track bank item inflow and outflow

--- src/main/java/com/supplybalance/SupplyBalancePlugin.java
+++ src/main/java/com/supplybalance/SupplyBalancePlugin.java
@@ -1,16 +1,22 @@
 package com.supplybalance;
-
-import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.gameval.InventoryID;
+import java.util.HashMap;
+import java.util.Map;
+import net.runelite.api.Item;
+import net.runelite.api.ItemContainer;
+import java.util.HashSet;
+import java.util.Set;
+import net.runelite.api.ItemComposition;
+//import net.runelite.client.config.ConfigManager;
 
 @Slf4j
 @PluginDescriptor(
@@ -20,36 +26,117 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class SupplyBalancePlugin extends Plugin
 {
+	//private static final String CONFIG_GROUP = "supply-balance";
+	//private static final String INFLOW_KEY_PREFIX = "inflow.";
+	//private static final String OUTFLOW_KEY_PREFIX = "outflow.";
+
+	private Map<Integer, Integer> previousBankSnapshot;
+	private final Map<Integer, Long> totalInflow = new HashMap<>();
+	private final Map<Integer, Long> totalOutflow = new HashMap<>();
+
 	@Inject
 	private Client client;
 
-	@Inject
-	private SupplyBalanceConfig config;
+	//@Inject
+	//private ConfigManager configManager;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.debug("Example started!");
+		previousBankSnapshot = null;
+		log.debug("Supply balance started");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.debug("Example stopped!");
+		previousBankSnapshot = null;
+		log.debug("Supply Balance stopped");
 	}
 
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		GameState gameState = gameStateChanged.getGameState();
+
+		if (gameState == GameState.LOGIN_SCREEN || gameState == GameState.HOPPING)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			previousBankSnapshot = null;
 		}
 	}
 
-	@Provides
-	SupplyBalanceConfig provideConfig(ConfigManager configManager)
+	@Subscribe
+	public void onItemContainerChanged(ItemContainerChanged event)
 	{
-		return configManager.getConfig(SupplyBalanceConfig.class);
+		if (event.getContainerId() != InventoryID.BANK)
+		{
+			return;
+		}
+		Map<Integer, Integer> currentBankSnapshot =
+				createBankSnapshot(event.getItemContainer());
+
+		if (previousBankSnapshot == null)
+		{
+			previousBankSnapshot = currentBankSnapshot;
+			log.debug("Stored initial bank snapshot containing {} item types",
+					previousBankSnapshot.size());
+			return;
+		}
+		Set<Integer> allItemIds = new HashSet<>(previousBankSnapshot.keySet());
+		allItemIds.addAll(currentBankSnapshot.keySet());
+
+		for (int itemId : allItemIds)
+		{
+			int previousQuantity = previousBankSnapshot.getOrDefault(itemId, 0);
+			int currentQuantity = currentBankSnapshot.getOrDefault(itemId, 0);
+			int change = currentQuantity - previousQuantity;
+
+			if (change > 0)
+			{
+				long existingInflow = totalInflow.getOrDefault(itemId, 0L);
+				long newInflow = existingInflow + change;
+				
+				totalInflow.put(itemId, newInflow);
+			}
+			else if (change < 0)
+			{
+				long amountOut = -change;
+				long existingOutFlow = totalOutflow.getOrDefault(itemId, 0L);
+				long newOutflow = existingOutFlow + amountOut;
+
+				totalOutflow.put(itemId, newOutflow);
+			}
+			if (change != 0)
+			{
+				long inflow = totalInflow.getOrDefault(itemId, 0L);
+				long outflow = totalOutflow.getOrDefault(itemId, 0L);
+				long net = inflow - outflow;
+
+				log.debug("Item {} changed by {} | In: {} Out: {} Net: {}",
+						itemId, change, inflow, outflow, net);
+			}
+		}
+
+		previousBankSnapshot = currentBankSnapshot;
+	}
+
+	private Map<Integer, Integer> createBankSnapshot(ItemContainer bank)
+	{
+		Map<Integer, Integer> snapshot = new HashMap<>();
+		for (Item item : bank.getItems())
+		{
+			int itemID = item.getId();
+			int quantity = item.getQuantity();
+			if (itemID < 0 || quantity <= 0)
+			{
+				continue;
+			}
+			ItemComposition itemComposition = client.getItemDefinition(itemID);
+			if (itemComposition.getPlaceholderTemplateId() != -1) {
+				continue;
+			}
+			snapshot.put(itemID, quantity);
+		}
+		return snapshot;
 	}
 }
